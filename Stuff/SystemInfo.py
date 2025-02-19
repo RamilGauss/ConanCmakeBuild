e834aa672d7a25fde7c695d0750ec4ea121de1de
RamilGauss <ramil2085@mail.ru>
2025-02-19 16:52:36 +0300

    Added Linux draft scripts.

--- Stuff/SystemInfo.py
+++ Stuff/SystemInfo.py
@@ -1,8 +1,16 @@
 import platform
+import subprocess
 
 def GetOperatingSystem() -> str:
     return platform.system()
 
 def GetInstalledCompiler() -> str:
-    return ""
+
+    # msvc
+    if 0:
+        result = subprocess.run(["cl", "--version"], capture_output=True, text=True, encoding='utf-8', errors='replace')
+    # clang
+    # gcc
+
+    return None
 
--- Stuff/ToolConfig.py
+++ Stuff/ToolConfig.py
@@ -6,10 +6,26 @@ class ToolInfo:
         self.operatingSystem = operatingSystem
 
 toolInfoObjList = [
-    { "name": "conan", "commands": ["pip", "install", "conan"], "operatingSystem": "any"},
-    { "name": "cmake", "commands": ["pip", "install", "cmake"], "operatingSystem": "any"},
-    { "name": "ninja", "commands": ["pip", "install", "Ninja"], "operatingSystem": "any"},
+    { "name": "conan", "commands": ["pip", "install", "conan"], "operatingSystem": "Windows"},
+    { "name": "cmake", "commands": ["pip", "install", "cmake"], "operatingSystem": "Windows"},
+    { "name": "ninja", "commands": ["pip", "install", "Ninja"], "operatingSystem": "Windows"},
+    { "name": "conan", "commands": ["pip3", "install", "conan"], "operatingSystem": "Linux"},
+    { "name": "cmake", "commands": ["pip3", "install", "cmake"], "operatingSystem": "Linux"},
+    { "name": "ninja", "commands": ["pip3", "install", "Ninja"], "operatingSystem": "Linux"},
     { "name": "msvc",  "commands": ["install_buildtools.ps1"],  "operatingSystem": "Windows"},
+
+    # Linux draft
+    # sudo apt-get install gcc
+    # sudo apt-get install g++
+    # sudo apt-get install clang
+
+    # sudo apt-get install pipx
+    # pipx ensurepath
+
+
+    # pipx install ninja
+    # pipx install cmake
+    # pipx install conan
 ]
 
 toolInfoList = []
